37893bd5389202582de4c7c13d0c2bb19f48d14a
Sky-wrc <you@example.com>
2026-06-02 13:11:17 +0300

    Изменения: + немного добавлено изменений в вид главной активности + добавлено разрешение на работу с интернетом в манифесте

--- app/src/main/java/com/amaykov/finalproject/MainActivity.java
+++ app/src/main/java/com/amaykov/finalproject/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.PopupMenu;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String PREFS_NAME = "PREFS_NAME";
     private static final String KEY_FIRST_RUN = "isFirstRun";
+
+    TextView mainText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +36,9 @@ public class MainActivity extends AppCompatActivity {
         if (menuButton != null) {
             bindMainMenu(menuButton);
         }
+        mainText = findViewById(R.id.main_text);
+        mainText.setText("android:text=\"Здесь будет основной текст/результаты.\\n\\nПлейсхолдер:\\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.\\n\\nProin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim.\\n\\nPellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede.\\n\\n(Текст переносится по ширине экрана и прокручивается.)\"");
+
     }
 
     private void bindMainMenu(View anchor) {
